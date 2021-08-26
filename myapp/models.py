9d73d271669bd8706cc871ec9ef46c1ec2b2b929
herucoro <herucoro3@gmail.com>
2021-08-27 06:47:15 +0900

    add model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    order_number = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+
+
+class History(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    seazon = models.CharField(max_length=1)
+    size = models.IntegerField()
+    quantity = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
